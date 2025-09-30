38b6a6e0f1abc5c77c876af28c5c0171d5f43e87
Mike Cann <mike.cann@gmail.com>
2025-10-01 07:40:13 +0800

    wip

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,12 +1,10 @@
-import { defineSchema, defineTable } from "convex/server";
-import { v } from "convex/values";
+import { defineSchema } from "convex/server";
 import { authTables } from "@convex-dev/auth/server";
 import { filesTable } from "./files/schema";
+import { processesTable } from "./processes/schema";
 
-// The schema is normally optional, but Convex Auth
-// requires indexes defined on `authTables`.
-// The schema provides more precise TypeScript types.
 export default defineSchema({
   ...authTables,
   files: filesTable,
+  processes: processesTable,
 });
